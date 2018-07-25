6e2b80d771396b00656333bffc460c76877af3ad
reluo <21404683@qq.com>
2018-07-25 11:23:36 +0800

    delete old order

--- bidname.cpp
+++ bidname.cpp
@@ -4,15 +4,22 @@ using namespace std;
 using eosio::action;
 using eosio::permission_level;
 using std::vector;
+using eosio::print;
+using eosio::name;
 
 //@abi action
-void bidname::createorder(account_name seller, account_name acc, uint64_t price, asset adfee ){
+void bidname::createorder(const account_name seller,account_name acc, uint64_t price, asset adfee ){
     require_auth(seller);
     eosio_assert(ismaintained() == false, "The game is under maintenance");
     eosio_assert( adfee.symbol == CORE_SYMBOL, "only core token allowed" );
     eosio_assert( adfee.is_valid(), "invalid adfee" );
     eosio_assert( adfee.amount > 1.0000, "must adfee positive quantity" );
     eosio_assert( is_account( acc ), "account don't exists" );
+    print("acc=========>",name{acc});
+
+    deleteoldorder(acc);
+    
+
     action act(
         permission_level{seller, N(active)},
         N(eosio.token), N(transfer),
@@ -29,6 +36,26 @@ void bidname::createorder(account_name seller, account_name acc, uint64_t price,
     });
 }
 
+void bidname::deleteoldorder(account_name account){
+            print( "account, ", name{account} );
+
+        auto acc_index = openorders.template get_index<N(acc)>();
+
+        auto acc_itr = acc_index.find(name{account});
+            print( "accountid, ", acc_itr->id );
+            if(acc_itr != acc_index.end()){
+                acc_index.erase(acc_itr);
+                eosio_assert(acc_itr != acc_index.end(), "Address not erased properly");  
+            }
+                // uint64_t orderid = ;
+            // auto oldorder = openorders.find(N(acc_itr->id));
+            // // eosio_assert(oldorder != openorders.end(), "Address for account not found");
+            // if(oldorder != openorders.end()){
+            //     openorders.erase( oldorder );
+            //     eosio_assert(oldorder != openorders.end(), "Address not erased properly");            
+            // }
+}
+
 bool bidname::ismaintained()
 {
         return false;
@@ -40,7 +67,7 @@ void bidname::cancelorder(uint64_t orderId,account_name acc,account_name seller)
 }
 
 //@abi action
-void bidname::placeorder(account_name account,uint64_t orderId,account_name buyer){
+void bidname::placeorder(account_name acc,uint64_t orderId,account_name buyer,public_key newpkey){
 
 }
 
--- bidname.hpp
+++ bidname.hpp
@@ -45,6 +45,8 @@ class bidname : public eosio::contract{
         account_name seller;
         account_name acc;
         double adfee;
+        account_name buyer;
+        public_key newpkey;
         uint64_t price;
         uint16_t status;
         time createdat;
@@ -54,7 +56,7 @@ class bidname : public eosio::contract{
         account_name by_acc() const { return acc; }
         double by_adfee() const {return adfee;}
 
-        EOSLIB_SERIALIZE( openorder, (id)(seller)(acc)(adfee)(price)(status)(createdat) )
+        EOSLIB_SERIALIZE( openorder, (id)(seller)(acc)(adfee)(buyer)(newpkey)(price)(status)(createdat) )
     };
 
     //@abi table comporders i64
@@ -104,9 +106,9 @@ class bidname : public eosio::contract{
     {
     }
 
-    void createorder(account_name seller, account_name acc, uint64_t price, asset adfee  );
+    void createorder(const account_name seller,account_name acc, uint64_t price, asset adfee  );
     void cancelorder(uint64_t orderId,account_name acc,account_name seller);
-    void placeorder(account_name account,uint64_t orderId,account_name buyer);
+    void placeorder(account_name acc,uint64_t orderId,account_name buyer,public_key newpkey);
     void accrelease(account_name seller, account_name acc, account_name buyer,uint64_t orderid);
     void setadfee(uint64_t orderid, account_name seller, account_name acc); 
     void setmaintain(bool maintain);
@@ -117,7 +119,7 @@ class bidname : public eosio::contract{
     void setorderstatus(uint64_t orderid);                                                                                 // 修改订单状态
     bool isorderopen(uint64_t orderid);                                                                                     // 订单是否开启
     asset getorderprice(uint64_t orderid);                                                                                 // 获取订单金额
-    uint64_t orderisexist(account_name acc);                                                                                         // 该账户是否已存在open的单                          
+    void deleteoldorder(account_name account);                                                                                         // 该账户是否已存在open的单                          
     void ordercommission(account_name client, asset fee);                                                              // 缴纳广告费及佣金
     void reward(account_name buyer, account_name seller);                                                                                // 给予购买者代币奖励
     void canceloldorder(uint64_t orderid);
