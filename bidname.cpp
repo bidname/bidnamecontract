830fdbed15e2cc0b5975fc45aa2cc59903f7228b
reluo <21404683@qq.com>
2018-07-26 01:29:36 +0800

    accrelease with a eosiosystem bug

--- bidname.cpp
+++ bidname.cpp
@@ -1,6 +1,7 @@
 #include "bidname.hpp"
 
 using namespace std;
+
 using eosio::action;
 using eosio::permission_level;
 using std::vector;
@@ -13,7 +14,7 @@ void bidname::createorder(const account_name seller,account_name acc, uint64_t p
     eosio_assert(ismaintained() == false, "The game is under maintenance");
     eosio_assert( adfee.symbol == CORE_SYMBOL, "only core token allowed" );
     eosio_assert( adfee.is_valid(), "invalid adfee" );
-    eosio_assert( adfee.amount > 1.0000, "must adfee positive quantity" );
+    eosio_assert( adfee.amount > 1, "must adfee positive quantity" );
     eosio_assert( is_account( acc ), "account don't exists" );
 
     deleteoldorder(acc);
@@ -64,20 +65,20 @@ void bidname::cancelorder(uint64_t orderid,account_name acc,account_name seller)
 }
 
 //@abi action
-void bidname::placeorder(account_name acc,uint64_t orderid,account_name buyer,public_key newpkey){
+void bidname::placeorder(account_name acc,uint64_t orderid,account_name buyer,eosio::public_key newpkey){
     require_auth(buyer);
-    auto acc_itr = openorders.find(orderid);
-    eosio_assert(acc_itr != openorders.end(), "don't find the order");
-    eosio_assert(acc_itr->status == OPEN, "order is locking");
-    eosio_assert(name{acc_itr->acc} == name{acc}, "order info is wrong");
+    auto order_itr = openorders.find(orderid);
+    eosio_assert(order_itr != openorders.end(), "don't find the order");
+    eosio_assert(order_itr->status == OPEN, "order is locking");
+    eosio_assert(name{order_itr->acc} == name{acc}, "order info is wrong");
 
     action act(
         permission_level{buyer, N(active)},
         N(eosio.token), N(transfer),
-        std::make_tuple(buyer, _self, asset(acc_itr->price), std::string("")));
+        std::make_tuple(buyer, _self, asset(order_itr->price), std::string("")));
     act.send();
 
-    openorders.modify(acc_itr,_self, [&]( auto& order ) {
+    openorders.modify(order_itr,_self, [&]( auto& order ) {
         order.buyer = buyer;
         order.newpkey = newpkey;
         order.status = LOCKING;
@@ -85,12 +86,63 @@ void bidname::placeorder(account_name acc,uint64_t orderid,account_name buyer,pu
 }
 
 //@abi action
-void bidname::accrelease(account_name seller, account_name acc, account_name buyer,uint64_t orderid){
+void bidname::accrelease(account_name seller, account_name acc, account_name buyer,uint64_t orderid,eosio::public_key newpkey){
+    print("i m here i right0");
 
+    require_auth(acc);
+
+    auto order_itr = openorders.find(orderid);
+    eosio_assert(order_itr != openorders.end(), "don't find the order");
+    eosio_assert(order_itr->status == LOCKING, "order is not locking");
+    eosio_assert(name{order_itr->acc} == name{acc}, "order info is wrong");
+    eosio_assert(name{order_itr->seller} == name{seller}, "order info is wrong");
+    eosio_assert(name{order_itr->buyer} == name{buyer}, "order info is wrong");
+    print("i m here i right1");
+    
+
+    action transferact(
+        permission_level{_self, N(active)},
+        N(eosio.token), N(transfer),
+        std::make_tuple(_self, seller, asset((order_itr->price)/2), std::string("")));
+    transferact.send();
+    // string str = order_itr->newpkey.data[0];
+    print("i m here acc=========>",name{order_itr->acc});
+    // print("i m here str=========>",str);
+    print("i m here seller=========>",name{order_itr->seller});
+    
+
+    bidname::key_weight keyweight = { .key = newpkey,  .weight = 1 };
+    bidname::authority ownerkey  = { .threshold = 1, .keys = { keyweight }, .accounts = {}, .waits = {} };
+    print("i m here i right2");
+
+    action updateauthact(
+        permission_level{acc, N(owner)},
+        N(eosio), N(updateauth),
+        std::make_tuple(acc, N(owner), "",ownerkey));
+    updateauthact.send();
+    print("i m here i right3");
+    
+    comporders.emplace(_self, [&](auto &order) {
+        order.id = openorders.available_primary_key();
+        order.seller = seller;
+        order.acc = acc;
+        order.price = order_itr->price;
+        order.buyer = buyer;
+        order.createdat = order_itr->createdat;
+        order.finishedat = now();
+    });
+
+    openorders.erase(order_itr);
+    
 }
 
 //@abi action
 void bidname::setadfee(uint64_t orderid, account_name seller, account_name acc){
+
+}
+
+//@abi action
+void bidname::cancelplace(account_name acc,uint64_t orderid,account_name buyer,account_name seller){
 
 }
 
@@ -109,8 +161,8 @@ void bidname::setroyalty(double royalty){
 }
 
 //@abi action
-void bidname::setreward(uint64_t reward){
+void bidname::setreward(int64_t reward){
 
 }
 
-EOSIO_ABI(bidname, (createorder)(cancelorder)(placeorder)(accrelease)(setadfee)(setmaintain)(setroyalty)(setreward))
+EOSIO_ABI(bidname, (createorder)(cancelorder)(placeorder)(accrelease)(setadfee)(setmaintain)(cancelplace)(setroyalty)(setreward))
--- bidname.hpp
+++ bidname.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <eosiolib/eosio.hpp>
+#include <eosiolib/public_key.hpp>
 #include <eosiolib/types.hpp>
 #include <eosiolib/asset.hpp>
 #include <string>
+
+using namespace eosio;
 
 using eosio::action;
 using eosio::asset;
@@ -14,10 +17,42 @@ using eosio::name;
 using eosio::permission_level;
 using eosio::print;
 using std::vector;
+// using eosio::public_key;
 
 class bidname : public eosio::contract{
   private:
 
+    struct key_weight 
+    {
+      eosio::public_key   key;
+      weight_type  weight;
+
+      // explicit serialization macro is not necessary, used here only to improve compilation time
+      EOSLIB_SERIALIZE( key_weight, (key)(weight) )
+    };
+
+    struct permission_level_weight
+    {
+      permission_level  permission ;
+      weight_type       weight;
+
+      // explicit serialization macro is not necessary, used here only to improve compilation time
+      EOSLIB_SERIALIZE( permission_level_weight, (permission)(weight) )
+   };
+
+    struct wait_weight 
+    {
+    uint32_t wait_sec;
+    weight_type weight;
+    };
+
+    struct authority   
+    {
+    uint32_t threshold;
+    vector<key_weight> keys;
+    vector<permission_level_weight> accounts;
+    vector<wait_weight> waits;
+    };
 
     typedef enum order_status
     {
@@ -31,7 +66,7 @@ class bidname : public eosio::contract{
         uint64_t id = 0;                        // 主键，使用available_primary_key生成
         bool maintained = false;                // 是否处于系统维护状态
         double royalty;
-        uint64_t reward;
+        int64_t reward;
 
         uint64_t primary_key()const { return id; }
 
@@ -44,9 +79,9 @@ class bidname : public eosio::contract{
         uint64_t id;
         account_name seller;
         account_name acc;
-        double adfee;
+        uint64_t adfee;
         account_name buyer;
-        public_key newpkey;
+        eosio::public_key newpkey;
         uint64_t price;
         uint16_t status;
         time createdat;
@@ -54,7 +89,7 @@ class bidname : public eosio::contract{
         uint64_t primary_key() const { return id; }
         account_name by_seller() const { return seller; }
         account_name by_acc() const { return acc; }
-        double by_adfee() const {return adfee;}
+        uint64_t by_adfee() const {return adfee;}
 
         EOSLIB_SERIALIZE( openorder, (id)(seller)(acc)(adfee)(buyer)(newpkey)(price)(status)(createdat) )
     };
@@ -66,14 +101,14 @@ class bidname : public eosio::contract{
         account_name seller;
         account_name acc;
         account_name buyer;
-        asset price;
+        uint64_t price;
         time createdat;
         time finishedat;
 
         uint64_t primary_key() const { return id; }
         account_name by_seller() const { return seller; }
         account_name by_acc() const { return acc; }
-        asset by_price() const {return price;}
+        uint64_t by_price() const {return price;}
 
         EOSLIB_SERIALIZE( comporder, (id)(seller)(acc)(buyer)(price)(createdat)(finishedat) )
     };
@@ -84,13 +119,13 @@ class bidname : public eosio::contract{
     typedef eosio::multi_index<N(openorders), openorder,
                                 indexed_by<N(seller), const_mem_fun<openorder, account_name, &openorder::by_seller>>,
                                 indexed_by<N(acc), const_mem_fun<openorder, account_name, &openorder::by_acc>>,
-                                indexed_by<N(adfee), const_mem_fun<openorder, double, &openorder::by_adfee>>>
+                                indexed_by<N(adfee), const_mem_fun<openorder, uint64_t, &openorder::by_adfee>>>
         open_orders_index;
 
     typedef eosio::multi_index<N(comporders), comporder,
                                 indexed_by<N(seller), const_mem_fun<comporder, account_name, &comporder::by_seller>>,
                                 indexed_by<N(acc), const_mem_fun<comporder, account_name, &comporder::by_acc>>,
-                                indexed_by<N(price), const_mem_fun<comporder, asset, &comporder::by_price>>>
+                                indexed_by<N(price), const_mem_fun<comporder, uint64_t, &comporder::by_price>>>
         comp_orders_index;
 
     global_set_index globalsets;
@@ -108,12 +143,13 @@ class bidname : public eosio::contract{
 
     void createorder(const account_name seller,account_name acc, uint64_t price, asset adfee  );
     void cancelorder(uint64_t orderid,account_name acc,account_name seller);
-    void placeorder(account_name acc,uint64_t orderid,account_name buyer,public_key newpkey);
-    void accrelease(account_name seller, account_name acc, account_name buyer,uint64_t orderid);
+    void placeorder(account_name acc,uint64_t orderid,account_name buyer,eosio::public_key newpkey);
+    void accrelease(account_name seller, account_name acc, account_name buyer,uint64_t orderid,eosio::public_key newpkey);
     void setadfee(uint64_t orderid, account_name seller, account_name acc); 
     void setmaintain(bool maintain);
     void setroyalty(double royalty);  
-    void setreward(uint64_t reward);
+    void setreward(int64_t reward);
+    void cancelplace(account_name acc,uint64_t orderid,account_name buyer,account_name seller);
 
   private:
     void setorderstatus(uint64_t orderid);                                                                                 // 修改订单状态
@@ -124,7 +160,7 @@ class bidname : public eosio::contract{
     void reward(account_name buyer, account_name seller);                                                                                // 给予购买者代币奖励
     void canceloldorder(uint64_t orderid);
     bool ismaintained();   
-    uint64_t getreward();
+    int64_t getreward();
     double getroyalty();
 
 };
